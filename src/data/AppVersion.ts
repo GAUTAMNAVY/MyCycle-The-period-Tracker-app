541c65dfb7f69d11a51bfa3e532a1c044d667388
Navy Gautam <141498497+GAUTAMNAVY@users.noreply.github.com>
2025-04-09 22:37:46 +0530

    Add files via upload

---
+++ src/data/AppVersion.ts
@@ -0,0 +1,79 @@
+import { isPlatform } from "@ionic/core";
+import semver from "semver";
+import { configuration } from "./AppConfiguration";
+
+export interface GithubReleaseAsset {
+  content_type: string;
+  browser_download_url: string;
+  
+}
+
+export interface GithubReleaseInfo {
+  html_url: string;
+  tag_name: string;
+  draft: boolean;
+  assets: GithubReleaseAsset[];
+  // NOTE: Other fields are not significant
+  //       You can see all fields with the following command:
+  //       `curl https://api.github.com/repos/IraSoro/peri/releases/latest`
+}
+
+export interface LatestReleaseInfo {
+  version: string;
+  htmlUrl: string;
+}
+
+async function getLatestReleaseInfo(): Promise<LatestReleaseInfo> {
+  const newVersionInfo = {
+    version: configuration.app.version,
+    htmlUrl: "",
+  } satisfies LatestReleaseInfo;
+
+  const response = await fetch(
+    "https://github.com/GAUTAMNAVY/MyCycle-The-period-Tracker-app",
+  );
+
+  const githubReleaseInfo = (await response.json()) as GithubReleaseInfo;
+
+  const apkUrls = githubReleaseInfo.assets.filter((asset) => {
+    return asset.content_type === "application/vnd.android.package-archive";
+  });
+
+  if (!apkUrls.length) {
+    throw new Error("Can't find apk files in assets list");
+  }
+
+  newVersionInfo.version = githubReleaseInfo.tag_name;
+  newVersionInfo.htmlUrl = githubReleaseInfo.html_url;
+
+  return newVersionInfo;
+}
+
+export async function isNewVersionAvailable(): Promise<boolean> {
+  if (
+    isPlatform("desktop") ||
+    isPlatform("mobileweb") ||
+    !isPlatform("android")
+  ) {
+    return false;
+  }
+
+  return semver.gt(
+    (await getLatestReleaseInfo()).version,
+    configuration.app.version,
+  );
+}
+
+export async function downloadLatestRelease() {
+  const latestRelease = await getLatestReleaseInfo();
+  window.open(latestRelease.htmlUrl, "_system", "location=yes");
+}
+
+export const homepageURL = "https://github.com/GAUTAMNAVY/MyCycle-The-period-Tracker-app";
+export function openGitHubPage() {
+  if (isPlatform("desktop")) {
+    window.open(homepageURL, "_blank")?.focus();
+    return;
+  }
+  window.open(homepageURL, "_system", "location=yes");
+}
